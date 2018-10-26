350b37b8c84b5f419bb2b18d717506a487086525
ottokafka <ottokafka@yahoo.com>
2018-10-27 04:48:18 +0800

    FontAwseme added

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,64 +1,91 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import { PropTypes } from 'prop-types';
-import { connect } from 'react-redux';
-import hammerImg from '../../img/hammer.png'
-import hands from '../../img/hands.png'
-import tech from '../../img/tech.png'
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+import {PropTypes} from 'prop-types';
+import {connect} from 'react-redux';
+
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faBalanceScale, faHandshake, faWifi} from '@fortawesome/free-solid-svg-icons'
+
 
 class Landing extends Component {
-  componentDidMount() {
-    if (this.props.auth.isAuthenticated) {
-      this.props.history.push('/dashboard');
+    componentDidMount() {
+        if (this.props.auth.isAuthenticated) {
+            this.props.history.push('/dashboard');
+        }
     }
-  }
 
-  render() {
-    return (
-        <div>
-      <div className="landing">
-        <div className="dark-overlay landing-inner text-light">
-          <div className="container">
-            <div className="row">
-              <div className="col-md-12 text-center">
-                <h1 className="display-3 mb-4">Members Connector</h1>
-                <p className="lead">
-                  {' '}
-                    CRESTO is a fintech solution on real estate investment market. CRESTO platform allows asset owners and developers tokenize their assets and hold initial sale of their tokenized securities. It is also a centralized platform for secondary trading of asset-backed tokens.
-                </p>
+    render() {
+        return (
+            <div>
+                <div className="landing">
+                    <div className="dark-overlay landing-inner text-light">
+                        <div className="container">
+                            <div className="row">
+                                <div className="col-md-12 text-center">
+                                    <h1 className="display-3 mb-4">Members Connector</h1>
+                                    <p className="lead">
+                                        {' '}
+                                        CRESTO is a fintech solution on real estate investment market. CRESTO platform
+                                        allows asset owners and developers tokenize their assets and hold initial sale
+                                        of their tokenized securities. It is also a centralized platform for secondary
+                                        trading of asset-backed tokens.
+                                    </p>
+                                    <hr/>
+
+                                    <Link to="/register" className="btn btn-lg btn-info mr-2">
+                                        Sign Up
+                                    </Link>
+                                    <Link to="/login" className="btn btn-lg btn-light">
+                                        Login
+                                    </Link>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <hr/>
                 <hr/>
 
-                <Link to="/register" className="btn btn-lg btn-info mr-2">
-                  Sign Up
-                </Link>
-                <Link to="/login" className="btn btn-lg btn-light">
-                  Login
-                </Link>
-              </div>
+
+                <div className="container">
+                    <div className="row">
+                        <div className="col text-center" >
+                            <FontAwesomeIcon icon={faBalanceScale} size="6x" className='landingIcons '/><p>Full legal compliance</p>
+
+
+                        </div>
+                        <div className="col text-center">
+                            <FontAwesomeIcon icon={faHandshake} size="6x" className='landingIcons'/><p>Cooperation instead of “disruption”</p>
+
+                        </div>
+                        <div className="col text-center">
+                            <FontAwesomeIcon icon={faWifi} size="6x" className='landingIcons'/><p>Clients’ advantage through technology</p>
+                        </div>
+                    </div>
+                </div>
+
+
+
+
+
+
+
+
+
+
+
+
             </div>
-          </div>
-        </div>
-      </div>
-
-          <div className=" text-white mt-5 p-4 text-center">
-              <img src={hammerImg} alt="hammer"/><img/>
-              <img src={hands} alt="hammer"/><img/>
-              <img src={tech} alt="hammer"/><img/>
-          </div>
-
-
-
-        </div>
-    );
-  }
+        );
+    }
 }
 
 Landing.propTypes = {
-  auth: PropTypes.object.isRequired
+    auth: PropTypes.object.isRequired
 };
 
 const mapStateToProps = state => ({
-  auth: state.auth
+    auth: state.auth
 });
 
 export default connect(mapStateToProps)(Landing);
