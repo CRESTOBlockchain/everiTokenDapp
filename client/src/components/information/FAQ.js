63d366b2d86639339b04d1edede1980753d9d38d
ottokafka <ottokafka@yahoo.com>
2018-10-28 22:26:22 +0800

    update FAQs

--- client/src/components/information/FAQ.js
+++ client/src/components/information/FAQ.js
@@ -10,30 +10,17 @@ class FAQ extends Component {
                 <div className="container">
                     <div className="row">
                         <div className="col-md-12 text-center">
-                            <h1>FAQ</h1>
-                            <hr/>
-                            <p>About CRESTO
-                                …
-                                …
-                                Account setting
-                                …
-                                …
-                                Risks
-                                …
-                                …
-                                Investing procedure
-                                …
-                                …
-                                STO procedure
-                                …
-                                …
-                                Profitability
-                                …
-                                …</p>
-                            <p>Coming sooner than you know</p>
-
+                            <h1>FAQs</h1>
+                            <ul class="list-group">
+                                <li class="list-group-item">About CRESTO</li>
+                                <li class="list-group-item">Account setting </li>
+                                <li class="list-group-item">   Risks</li>
+                                <li class="list-group-item">Investing procedure </li>
+                                <li class="list-group-item">STO procedure </li>
+                                <li class="list-group-item">  Profitability</li>
+                            </ul>
+                            <p>Coming sooner than you know!</p>
                         </div>
-
                     </div>
                 </div>
             </div>
