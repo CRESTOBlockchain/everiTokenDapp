f1c0f45c6515b2ad05bb1633f09fb03757d2412d
ottokafka <ottokafka@yahoo.com>
2018-10-29 11:17:02 +0800

    team update

--- client/src/components/information/Team.js
+++ client/src/components/information/Team.js
@@ -11,31 +11,30 @@ class Team extends Component {
             <div>
                 <h1 className="display-3 mb-4 text-center">TEAM</h1>
                 <div className="container">
-                    <div className="row">
-                        <div className="col text-center" >
+                    <div className="d-flex flex-wrap">
+                        <div className=" text-center" >
                             <img src='https://user-images.githubusercontent.com/21117852/47626168-be41dc80-db64-11e8-8b0e-40f71bc09495.png'/>
                         </div>
-                        <div className="col text-center">
+                        <div className=" text-center">
                             <img src='https://user-images.githubusercontent.com/21117852/47626182-cc8ff880-db64-11e8-8ad9-923fd10fb9db.png'/>
                         </div>
-                        <div className="col text-center">
+                        <div className=" text-center">
                             <img src='https://user-images.githubusercontent.com/21117852/47626197-d7e32400-db64-11e8-963c-ef489b7be89c.png'/>
                         </div>
-                        <div className="col text-center">
+                        <div className=" text-center">
                             <img src='https://user-images.githubusercontent.com/21117852/47626208-e3364f80-db64-11e8-912d-f2b43c198ce9.png'/>
                         </div>
-                    </div>
-                    <div className="row">
-                        <div className='col text-center'>
+
+                        <div className=' text-center'>
                             <img src="https://user-images.githubusercontent.com/21117852/47626225-f3e6c580-db64-11e8-996c-9747c1401f3e.png"/>
                         </div>
-                        <div className='col text-center'>
+                        <div className=' text-center'>
                             <img src="https://user-images.githubusercontent.com/21117852/47626242-05c86880-db65-11e8-934a-e7fdcf91e96f.png"/>
                         </div>
-                        <div className='col text-center'>
+                        <div className=' text-center'>
                             <img src="https://user-images.githubusercontent.com/21117852/47626258-14af1b00-db65-11e8-9bb1-6ad387bd11a1.png"/>
                         </div>
-                        <div className='col text-center'>
+                        <div className=' text-center'>
                             <img src="https://user-images.githubusercontent.com/21117852/47626269-2690be00-db65-11e8-97a1-805dae774e8d.png"/>
                         </div>
                     </div>
